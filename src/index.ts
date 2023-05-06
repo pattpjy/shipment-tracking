4b7c5ea02e30b6fbd4613a25322d29d2836ce681
Patt Sookmark <pijiya@sookmark.com>
2023-05-05 22:49:59 -0600

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const createDiv = function () {
+  const div = document.createElement("div");
+  const statement = document.createTextNode("Cargo Tracking App");
+  div.append(statement);
+  document.body.appendChild(div);
+};
+createDiv();
